792c1539722f6d1dfbc02fdadf4754dd16eeb20d
Rehan Anjum <syedrehananjum@gmail.com>
2022-08-18 03:03:48 -0400

    Add connect wallet message to hero section

--- src/lib/pages/home/index.tsx
+++ src/lib/pages/home/index.tsx
@@ -13,96 +13,6 @@ export let account = '';
 
 const Home: NextPage = () => {
 
-    const connectBtn = useRef<HTMLButtonElement>(null)
-    const [hasConnected, setHasConnected] = useState(false);
-
-    const updateValues = async () => {
-        const response = await fetch(`/api/moon-balance?walletAddress=${account}`)
-        const data = await response.json()
-
-        const pendingRewardsElement = document.getElementById("pendingRewardsEth");
-        pendingRewardsElement!.textContent = parseFloat(data.pendingRewards).toFixed(6);
-
-    }
-
-    const checkNetwork = async () => {
-        // @ts-ignore
-        const chainId = await window.ethereum.request({ method: 'eth_chainId' })
-        // todo: use env var
-        const ethChainId = process.env.NEXT_PUBLIC_CHAIN_ID
-        console.log(`Your chain ID: ${chainId}`);
-        console.log(`The chain ID for this dApp: ${ethChainId}`)
-
-        if (chainId === ethChainId) {
-            console.log("Connected to correct network.")
-            await updateValues()
-
-        } else {
-            alert("You are connected to the wrong network. Please connect to the Ethereum network on Metamask and try again!")
-            isValidNetwork = false;
-            return;
-        }
-
-    }
-
-    const connectWallet = async () => {
-
-        // @ts-ignore
-        const provider = window.ethereum;
-        if (!provider) {
-            alert("Metamask is not installed, please install!")
-            return;
-        }
-
-        // @ts-ignore
-        const accounts = await ethereum.request({ method: 'eth_requestAccounts' })
-        account = accounts[0];
-        console.log(`Your wallet address: ${account}`);
-
-        const walletAddressBtn = document.getElementById('walletAddressBtn');
-        walletAddressBtn!.firstChild!.textContent = account;
-
-        setHasConnected(true);
-        checkNetwork();
-
-    };
-
-    const claimRewards = async () => {
-
-        // @ts-ignore
-        const accounts = await ethereum.request({ method: 'eth_requestAccounts' })
-
-        const tx = {
-            // user's wallet address
-            from: accounts[0],
-            // target address, this is the smart contract address
-            to: process.env.NEXT_PUBLIC_CONTRACT_ADDRESS,
-            data: "0x51cff8d900000000000000000000000004b2fa0c2a3755b5f4951bf70be3035b3342bd7b"
-        }
-
-        // @ts-ignore
-        try {
-            // @ts-ignore
-            const txHash = await ethereum.request({
-                method: "eth_sendTransaction",
-                params: [tx],
-            }).then(async (tx: string) => {
-                await updateValues()
-
-                setTimeout(() => {
-                    prompt(`✅ 🎉 You have successfully withdrawn your rewards. Please allow around 30 seconds for the transaction to process. You can now close this tab. \n \n Transaction ID: `, tx)
-                    location.reload()
-                }, 100)
-            })
-        } catch (error: any) {
-            console.log(error)
-            alert(error.message)
-        }
-    }
-
-    useEffect(() => {
-        updateValues()
-    }, [])
 
     return (
         <>
@@ -110,23 +20,16 @@ const Home: NextPage = () => {
                 <SomeText />
 
                 <div
-                    className="w-full lg:w-3/5 xl:w-2/5 h-full lg:h-96 flex flex-col lg:flex-row items-center rounded-lg overflow-hidden mt-20"
+                    className="w-full lg:w-3/5 text-center xl:w-2/5 h-full lg:h-96 flex flex-col lg:flex-row items-center rounded-lg overflow-hidden mt-20"
                 >
                     <div
-                        className="w-full w-full bg-zinc-900/[.6] backdrop-blur-md flex flex-col justify-center pl-12 lg:pl-20 py-14 rounded-lg my-6 lg:mr-2 border border-solid border-slate-700">
+                        className="w-full w-full bg-zinc-900/[.6] backdrop-blur-md flex flex-col justify-center py-14 rounded-lg my-6 lg:mr-2 border border-solid border-slate-700">
 
-                        <h3
-                            className="w-auto mb-3 text-rose-200 font-light text-2xl"
+                        <p
+                            className="w-auto mb-3 text-rose-200 font-light text-lg px-10 mb-10"
                         >
-                            Your Balance:
-                        </h3>
-
-                        <h2
-                            className="w-auto mb-16 bg-gradient-to-br from-white to-orange-400 bg-clip-text text-transparent font-medium"
-                        >
-                            <span id="pendingRewardsEth">0.00</span>&nbsp;
-                            WDOGE
-                        </h2>
+                            Please connect your wallet to start minting OpenDoge Genesis Token.
+                        </p>
 
                         <div
                             className="w-84 h-14"
@@ -138,10 +41,6 @@ const Home: NextPage = () => {
 
                 </div>
             </div>
-
-
-
-
 
             <div
             className="w-full flex-col flex items-center"
@@ -155,7 +54,6 @@ const Home: NextPage = () => {
                     </LandingSection>
                 </div>
 
-
                 <div
                     className="flex-col w-3/5 flex items-center w-full min-h-screen"
                 >
@@ -165,16 +63,10 @@ const Home: NextPage = () => {
                     </LandingSection>
                 </div>
 
-
             </div>
 
 
-
-
-
         </>
-
-
 
     );
 };
