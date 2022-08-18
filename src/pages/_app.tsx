33229849ef05982efd72616c7ebab789852fbb99
Rehan Anjum <syedrehananjum@gmail.com>
2022-08-18 18:00:11 -0400

    Add dogechain to rainbowkit, change rainbowkit avatar styling

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,14 +3,17 @@ import type { AppProps } from "next/app";
 import defaultSEOConfig from "../../next-seo.config";
 import Layout from "lib/components/layout";
 import "lib/styles/globals.css";
-
-
 import '@rainbow-me/rainbowkit/styles.css';
+import { jsonRpcProvider } from 'wagmi/providers/jsonRpc';
+// import { Chain } from "wagmi";
+
 import {
     darkTheme,
     getDefaultWallets,
     RainbowKitProvider,
     Theme,
+    Chain,
+    AvatarComponent
 } from '@rainbow-me/rainbowkit';
 import {
     chain,
@@ -18,17 +21,32 @@ import {
     createClient,
     WagmiConfig,
 } from 'wagmi';
-import { alchemyProvider } from 'wagmi/providers/alchemy';
-import { publicProvider } from 'wagmi/providers/public';
 import merge from 'lodash.merge';
 
+const dogechain: Chain = {
+    id: 2_000,
+    name: 'DogeChain',
+    network: 'dogechain',
+    iconUrl: '/img/opendogelogo.png',
+    iconBackground: 'white',
+    nativeCurrency: {
+      decimals: 18,
+      name: 'Wrapped Doge',
+      symbol: 'wDOGE',
+    },
+    rpcUrls: {
+      default: 'https://rpc01-sg.dogechain.dog',
+    },
+    blockExplorers: {
+        default: { name: 'DC Explorer', url: 'https://explorer.dogechain.dog/' },
+        etherscan: { name: 'DC Explorer', url: 'https://explorer.dogechain.dog/' },
+      },
+    testnet: false,
+  }
 
 const { chains, provider } = configureChains(
-    [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum],
-    [
-        alchemyProvider({ apiKey: process.env.ALCHEMY_ID }),
-        publicProvider()
-    ]
+    [dogechain],
+    [jsonRpcProvider({ rpc: chain => ({ http: chain.rpcUrls.default }) })]
 );
 
 const { connectors } = getDefaultWallets({
@@ -42,6 +60,25 @@ const wagmiClient = createClient({
     provider
 })
 
+const CustomAvatar: AvatarComponent = ({ address, ensImage, size }) => {
+    const color = (address);
+    return ensImage ? (
+      <img
+        src={ensImage}
+        width={size}
+        height={size}
+        style={{ borderRadius: 999 }}
+      />
+    ) : (
+        <img
+        src="/img/opendogeavatar.png"
+        width={size}
+        height={size}
+        style={{ borderRadius: 999 }}
+      />
+    );
+  };
+  
 
 const myTheme = merge(darkTheme({
     fontStack: 'system',
@@ -69,6 +106,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
             <RainbowKitProvider
                 chains={chains} modalSize="compact"
                 theme={myTheme}
+                avatar={CustomAvatar}
             >
                 <Layout>
                     <DefaultSeo {...defaultSEOConfig} />
