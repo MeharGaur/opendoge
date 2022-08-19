992a833b9ee4b016a9278e1a1b5c7ada130ec241
Mehar Gaur <hello@mehar.ca>
2022-08-18 20:35:59 -0400

    Add animated counter to mint UI using svelte spring store

--- src/lib/components/ui/counter/index.tsx
+++ src/lib/components/ui/counter/index.tsx
@@ -1,37 +1,56 @@
-import { useState } from "react";
+import styles from "./index.module.scss"
+import { spring } from "svelte/motion"
 
 
 export function Counter() {
-    const [count, setCount] = useState(0)
+    let count = 0
+    const displayedCount = spring(0)
+
+    displayedCount.subscribe((value) => {
+        if (typeof window != "undefined") {
+            const digits = document.getElementById('counterDigits')!
+
+            digits.style.transform = `translate(0, ${100 * modulo(value)}%)`
+            digits.firstChild!.textContent = String(Math.floor(value + 1))
+            digits.lastChild!.textContent = String(Math.floor(value))
+        }
+    })
+
+    function updateCount(direction: number) {
+        count += direction
+        displayedCount.set(count)
+    }
 
     return (
         <div className="w-full h-32 bg-blue-500 mb-8">
-            {/* <div className={styles.counter}>
-                <button onClick={() => (setCount(count - 1))} aria-label="Decrease the counter by one">
+            <div className={styles.counter}>
+                <button onClick={() => updateCount(-1)} aria-label="Decrease the counter by one">
                     <svg aria-hidden="true" viewBox="0 0 1 1">
                         <path d="M0,0.5 L1,0.5" />
                     </svg>
                 </button>
 
-                <div class="counter-viewport">
-                    <div class="counter-digits" style="transform: translate(0, {100 * offset}%)">
-                        <strong class="hidden" aria-hidden="true">{Math.floor($displayed_count + 1)}</strong>
-                        <strong>{Math.floor($displayed_count)}</strong>
+                <div className={styles.counterViewport}>
+                    <div id="counterDigits" className={styles.counterDigits}>
+                        <strong className={styles.hidden} aria-hidden="true">0</strong>
+                        <strong>0</strong>
                     </div>
                 </div>
 
-                <button on:click={() => (count += 1)} aria-label="Increase the counter by one">
+                <button onClick={() => updateCount(1)} aria-label="Increase the counter by one">
                     <svg aria-hidden="true" viewBox="0 0 1 1">
                         <path d="M0,0.5 L1,0.5 M0.5,0 L0.5,1" />
                     </svg>
                 </button>
-            </div> */}
+            </div>
         </div>
     )
 }
 
 
-function modulo(n: number, m: number) {
+
+
+function modulo(n: number, m: number = 1) {
     // handle negative numbers
     return ((n % m) + m) % m;
 }
